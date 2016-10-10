9917cb9780d76d54a1308e3b0835663fd4fd61a3
YagoPeixoto <yagopeixoto19@gmail.com>
2016-10-10 11:17:48 -0300

    helloNode usando tagshtml

--- helloNode.js
+++ helloNode.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function(req,res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' }); 
-  res.end('Olá mundo!');
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });//se mudar o text/plain para text/html voce pede usar tags html dentro do codigo
+  res.end('<h1>Olá mundo!</h1>');
 }).listen(8003);
 console.log('Servidor iniciado em localhost:8003. Ctrl+C para encerrar…');
