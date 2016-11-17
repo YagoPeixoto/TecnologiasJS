8b3452ebdcb067f995dd535cde4b9cfe65453a78
YagoPeixoto <yagopeixoto19@gmail.com>
2016-11-16 23:58:42 -0200

    projeto noticiasIF

---
+++ APPNoticias/cordova/anuncios/www/js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var spa = angular.module('anuncioApp', ['ngRoute', 'anuncioAppControllers']);
+
+spa.config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+        templateUrl: 'partials/splashscreen.html',
+        controller: 'SplashScreenCtrl'
+        //animation: 'animacao-avancar'
+    })
+    .when('/anuncios', {
+        templateUrl: 'partials/anuncios.html',
+        controller: 'AnunciosCtrl'
+        //animation: 'animacao-avancar'
+    })
+    .otherwise({ redirectTo: '/'});
+});
